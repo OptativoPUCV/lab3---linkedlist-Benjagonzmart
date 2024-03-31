9105e584624c6e9c09a1d8644f52bdb22dfd1df8
Your Name <you@example.com>
2024-03-31 00:00:08 +0000

    new try

--- list.c
+++ list.c
@@ -114,12 +114,42 @@ void pushFront(List * list, void * data)
 }
 
 
-void pushBack(List * list, void * data) {
+void pushBack(List * list, void * data)
+{
     list->current = list->tail;
     pushCurrent(list,data);
 }
 
-void pushCurrent(List * list, void * data) {
+void pushCurrent(List * list, void * data) 
+{
+  Node * new = createNode(data);
+  if(list->current == NULL)
+  {
+    list->head = new;
+    list->tail = new;
+    
+  }
+  else
+  {
+    new->next = list->current->next;
+    new->prev = list->current;
+    if(list->current->next != NULL)
+    {
+      list->current->next->prev = new;
+      
+    }
+    list->current->next = new;
+    if(list->current == list->tail)
+    {
+      list->tail = new;
+    }
+  }
+  
+  
+  
+      
+  
+  
 }
 
 void * popFront(List * list) {
